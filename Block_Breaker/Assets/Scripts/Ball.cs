103c3f2c18e2e38f082debdda080f7b45dc647e9
Harley Garcia <hagarcia99@outlook.com>
2020-06-29 22:38:03 -0500

    Paddle doesn't go off screen and ball is stuck on paddle

---
+++ Block_Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // config parameters
+    [SerializeField] Paddle paddle1;
+    // state
+    Vector2 paddleToBallVector;
+
+    void Start() {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    void Update() {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
--- Block_Breaker/Assets/Scripts/Paddle.cs
+++ Block_Breaker/Assets/Scripts/Paddle.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Paddle : MonoBehaviour {
 
     [SerializeField] float  screenWidthInUnits = 16;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     
 
     void Update() {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2 (mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
